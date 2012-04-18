b76af823b92cd4e0761eb79b20ce143dbfbb6907
Alex Murphy <alexander.p.murphy.13@dartmouth.edu>
2012-04-17 23:12:37 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mixeight::Application.config.secret_token = '9bc396cace252992bddd8d6e1eab40141597bfb026de4c16f8f156d75c0671745718d6b18842cc78b33bf2bf3cc6f291b33f62c6314810532fa6b838bb852615'
