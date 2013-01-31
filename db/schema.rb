ae6d26b598dbdc8709209fef5898eae7d3dd0948
Alex <alex@ubuntu.(none)>
2013-01-31 16:02:24 -0500

    remove voting for now, add upload button

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120509053713) do
+ActiveRecord::Schema.define(:version => 20120510025931) do
 
   create_table "songs", :force => true do |t|
     t.integer  "upvotes"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(:version => 20120509053713) do
     t.datetime "updated_at",      :null => false
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
